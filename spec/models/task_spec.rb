d962f1b7ed61d45f239d0c9e0c89c8ecc2eb2510
Busik <Busik96@gmail.com>
2020-02-17 19:01:37 +0100

    specs_for_models

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -22,5 +22,11 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:description) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,5 +28,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:authorizations) }
+    it { is_expected.to have_many(:tasks) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
