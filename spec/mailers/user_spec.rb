6ac203fd4513aae94c84475e97190fe38820dc56
Busik <Busik96@gmail.com>
2020-02-19 11:34:20 +0100

    rest specs

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -4,16 +4,18 @@ require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   describe 'task_reminder' do
-    let(:mail) { UserMailer.task_reminder }
+    let(:mail) { UserMailer.task_reminder(task) }
+    let(:task) { create :task, user: user }
+    let(:user) { create :user }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Task reminder')
-      expect(mail.to).to eq(['to@example.org'])
+      expect(mail.subject).to eq('!!!TASK REMINDER!!!')
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(['from@example.com'])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.body.encoded).to match(user.name)
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/app/channels'
   add_filter '/app/controllers/registrations_controller.rb'
+  add_filter 'app/helpers/application_helper.rb'
+  add_filter 'app/policies/application_policy.rb'
 end
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
